8a3687b7d8b493d98ad5546243e58ad849482b84
shaman2016scratch <g.ilkh@yandex.ru>
2026-09-07 14:48:47 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ bot.command('help', async (ctx) => {
         }
         await updateIndex(data)
     }
-    ctx.reply("<b>Команды:</b>\n/info [id] - информация о пользователе\nИсходный код: https://github.com/shaman2016scratch/moders-tg-bot", { parse_mode: "HTML" })
+    ctx.reply("<b>Команды:</b>\n/info [id] - информация о выбранном пользователе\n/me - ваша информация\nИсходный код: https://github.com/shaman2016scratch/moders-tg-bot", { parse_mode: "HTML" })
 })
 
 bot.command('info', async (ctx) => {
@@ -140,6 +140,47 @@ bot.command('me', async (ctx) => {
     const userId = ctx.message.from.id
     const user = data.users[userId.toString()]
     ctx.reply(`<b>Информация о пользователе</b>\nID: ${user.id || 0}\nUsername: ${user.username}\nИмя: ${user.firstName}\nВ боте с ${new Date(user.joined)}\nРепутация: ${user.reputation}`, { parse_mode: "HTML" })
+})
+
+bot.command('addbotadmin', async (ctx) => {
+    const data = await getIndex()
+    if (!Object.keys(data.users).includes(ctx.message.from.id.toString())) {
+        data.users[ctx.message.from.id.toString()] = {
+            id: ctx.message.from.id,
+            username: ctx.message.from.username,
+            firstName: ctx.message.from.first_name,
+            language: "en",
+            joined: new Date(),
+            reputation: 0
+        }
+        await updateIndex(data)
+    }
+    if (data.admins.includes(ctx.message.from.id)) {
+        const userId = ctx.message.reply_to_message ? ctx.message.reply_to_message.from.id : ctx.message.text.replace("/info ", "")
+        data.admins.push(userId)
+        await updateIndex(data)
+    }
+})
+
+bot.command('setreputation', async (ctx) => {
+    const data = await getIndex()
+    if (!Object.keys(data.users).includes(ctx.message.from.id.toString())) {
+        data.users[ctx.message.from.id.toString()] = {
+            id: ctx.message.from.id,
+            username: ctx.message.from.username,
+            firstName: ctx.message.from.first_name,
+            language: "en",
+            joined: new Date(),
+            reputation: 0
+        }
+        await updateIndex(data)
+    }
+    if (data.admins.includes(ctx.message.from.id)) {
+        const userId = ctx.message.reply_to_message ? ctx.message.reply_to_message.from.id : ctx.message.text.split(" ")[1]
+        const rep = ctx.message.reply_to_message ? Number(ctx.message.text.split(" ")[1]) : Number(ctx.message.text.split(" ")[2])
+        data.users[userId.toString()].reputation = rep
+        await updateIndex(data)
+    }
 })
 
 bot.launch()
